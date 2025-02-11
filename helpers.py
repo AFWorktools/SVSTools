542f750c54a06ac5735ecba4414477573c1dfa8e
AngusFaanhof <github@adfpm.anonaddy.com>
2025-02-11 16:27:35 +0100

    Implemented compare page

--- helpers.py
+++ helpers.py
@@ -1,27 +1,36 @@
 import datetime
 
-types = {
+sopu_map = {
     "8718906547001": "01 RDC",
     "8714252002010": "06 Bloemen",
     "8710400000075": "09 LDC",
     "8711382000046": "10 Note",
+    "8712423007277": "12 Communicatie Containers e/o Posttassen",
     "8712423014534": "14 Winkel inventaris",
 }
+
+months = ['jan', 'feb', 'mar', 'apr', 'mei', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec']
+
 
 def parse_table_row(row):
     row_data = row.split("\t")
 
-    week = datetime.datetime.now().isocalendar().week
-    day = datetime.datetime.now().isocalendar().weekday
+    current_week = datetime.datetime.now().isocalendar().week
+    current_day = datetime.datetime.now().isocalendar().weekday
+
     store = row_data[2]
-    date, time = row_data[1].split(" ")
-    date = date.replace(".", "-")
     amount = row_data[3]
-    type = types.get(row_data[4], "Unknown")
+    type = sopu_map.get(row_data[4])
+
+    date, time = row_data[1].split(" ")
+    time = time[:-3] # remove seconds and :
+    
+    date = date.split('.')
+    date = f"{date[0]}-{months[int(date[1]) - 1]}" # 11-01-2023 -> 11-jan fromat
 
     return {
-        "week": week,
-        "day": day,
+        "week": current_week,
+        "day": current_day,
         "store": store,
         "time": time,
         "amount": amount,
@@ -29,11 +38,59 @@ def parse_table_row(row):
         "type": type,
     }
 
-
 def process_table_input(table_input):
     rows = []
 
     for row in table_input.split("\n"):
         rows.append(parse_table_row(row))
-
+ 
     return rows
+
+def hashmap_tool(tool_data):
+    hashed_rows = {}
+
+    # first two rows are headers
+    for row in tool_data.split("\n")[2:]:
+        row_data = row.split("\t")
+
+        store = row_data[2]
+        time = row_data[3]
+        amount = row_data[4]
+        date = row_data[5]
+        type = row_data[6]
+
+        finished = row_data[12] == 'j\r'
+
+        hash = f"{store}{time}{amount}{date}{type}"
+        hashed_rows[hash] = finished
+    
+    return hashed_rows
+
+def hashmap_vision(vision_data):
+    hashed_rows = {}
+
+    for row in vision_data.split("\n"):
+        parsed_row = parse_table_row(row)
+
+        store = parsed_row['store']
+        time = parsed_row['time']
+        amount = parsed_row['amount']
+        date = parsed_row['date']
+        type = parsed_row['type']
+
+        hash = f"{store}{time}{amount}{date}{type}"
+        hashed_rows[hash] = parsed_row
+    
+    return hashed_rows
+
+def filter_table(vision_input, tool_input):
+    filtered_rows = []
+
+    tool_hashmap = hashmap_tool(tool_input)
+    vision_hashmap = hashmap_vision(vision_input)
+
+    for hash, row in vision_hashmap.items():
+        if not tool_hashmap.get(hash):
+            filtered_rows.append(row)
+
+    return filtered_rows
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from helpers import process_table_input
+from helpers import process_table_input, filter_table
 
 app = Flask(__name__)
 
@@ -15,6 +15,17 @@ def unassigned():
         return render_template('unassigned.html', table=processed_table)
 
     return render_template('unassigned.html')
+
+@app.route('/compare', methods=['GET', 'POST'])
+def compare_excel():
+    if request.method == 'GET':
+        return render_template('compare.html')
+    
+    vision_data = request.form['visionInput']
+    tool_data = request.form['toolInput']
+    filtered_table = filter_table(vision_data, tool_data)
+
+    return render_template('compare.html', table=filtered_table)
 
 if __name__ == '__main__':
     app.run(debug=True)
